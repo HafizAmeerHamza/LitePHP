22e93f1e52e4a9920894acf11aa3d852e4cb569f
hafizameerhamza <hafizameerhamza1@gmail.com>
2025-10-02 23:42:20 +0500

    .env example

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use App\Core\Route;
 use App\Controllers\HomeController;
 
-// Route::get('/', 'HomeController@index')->name('home');
 Route::get('/', [HomeController::class, 'index'])->name('home');
